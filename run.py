0bb776f413ec4367a88a912c3661617f724e2615
Dario <dariocovella@gmail.com>
2023-08-23 22:17:13 +0100

    Add requirements for deployment

--- run.py
+++ run.py
@@ -4,7 +4,6 @@
 
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -42,7 +41,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: \n")
 
         # We’ll define a new variable called sales_data  
         # and use the split() method on our data string, to break it up at the commas. 

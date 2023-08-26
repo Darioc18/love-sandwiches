5a5f7082c40ef8bdc7ee75f03b660b78c494baa2
Dario <dariocovella@gmail.com>
2023-08-26 15:31:54 +0100

    Print out the recommended stock values for each sandwich type

--- run.py
+++ run.py
@@ -163,7 +163,25 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 print('Welcome to Love Sandwiches Data Automation')
-main()
+stock_data = main()
 
+def get_stock_values(data):
+    """
+    Calculate and print out the recommended stock values for each sandwich type
+    """
+    # stock = SHEET.worksheet("stock").get_all_values()
+    # headings = stock[0]
+    
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+    
+    print("Make the following number of sandwiches for next market:")
+    
+    return {heading: data for heading, data in zip(headings, data)}
+
+    
+    
+stock_values = get_stock_values(stock_data)  
+print(stock_values)
